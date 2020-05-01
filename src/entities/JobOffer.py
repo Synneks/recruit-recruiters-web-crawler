7ba6b9bdc31869ead806b99e93237acbcd9ce87a
Denis Eneotescu <Deniseneotescu@gmail.com>
2020-05-01 20:16:34 +0300

    Added scrapper for indeed details

--- src/entities/JobOffer.py
+++ src/entities/JobOffer.py
@@ -3,29 +3,19 @@ import json
 
 class JobOffer(json.JSONEncoder):
 
-    def __init__(self, job_title, company_name, application_link):
+    def __init__(self, job_title, company_name, site, application_link):
         self.job_title = job_title
         self.company_name = company_name
-        self.application_link = application_link
-
-    def get_job_title(self):
-        return self.job_title
-
-    def get_company_name(self):
-        return self.company_name
-
-    def get_application_link(self):
-        return self.application_link
-
-    def set_application_link(self, application_link):
+        self.site = site
         self.application_link = application_link
 
     def __str__(self):
-        return f"{self.job_title} | {self.company_name} | {self.application_link}"
+        return f"{self.job_title} | {self.company_name} | {self.site} | {self.application_link}"
 
     def __repr__(self):
         return {"Job Title": self.job_title,
                 "Company Name": self.company_name,
+                "Site": self.site,
                 "Application Link": self.application_link}
 
     def default(self, o):
